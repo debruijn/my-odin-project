3dcd83fea5fa263c1c338cbf6c94207bda0de5ec
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-14 10:42:16 -0400

    Add getComputerChoice() func

--- js-exercises/foundations/rock-paper-scissors/main.js
+++ js-exercises/foundations/rock-paper-scissors/main.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let rand = Math.random()
+    if (rand < 1/3) {
+        return "R"
+    } else if (rand < 2/3) {
+        return "S"
+    } else {
+        return "P"
+    }
+}
