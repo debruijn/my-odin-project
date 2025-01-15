fd4ad048c7c40490bdcb9e2a3075d0f9f2964c16
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-15 16:43:15 -0400

    Add setup of etch a sketch challenge to complete later

---
+++ js-exercises/foundations-extra/03_etch_a_sketch/main.js
@@ -0,0 +1,8 @@
+// Plan:
+// - Get container reference
+// - Create square divs and add to container as a 4x4 grid (or flexible?)
+// - Use flexbox in specifying how to show them
+// - Add the hover effect to change the color when mouse pointer moves over a grid
+// - Allow for overwrite of grid with new grid based on numeric input
+// - Make grid random color when going through grid
+// - Make square darker by 10%
