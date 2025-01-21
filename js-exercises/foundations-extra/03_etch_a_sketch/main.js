1682fb6338e4840c143f0c7649ebf9d62bc705bb
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-21 16:17:25 -0400

    Add simple stretched grid

--- js-exercises/foundations-extra/03_etch_a_sketch/main.js
+++ js-exercises/foundations-extra/03_etch_a_sketch/main.js
@@ -6,3 +6,34 @@
 // - Allow for overwrite of grid with new grid based on numeric input
 // - Make grid random color when going through grid
 // - Make square darker by 10%
+
+const container = document.querySelector("#container");
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+container.style.justifyContent = 'center';
+container.style.flex = 'auto';
+container.style.height = '100%';
+let size = 16;
+
+
+
+for (let i = 0; i < size; i++) {
+    let row = document.createElement('div')
+    row.className = 'row';
+    row.style.display = 'flex';
+    row.style.flex = 'auto';
+    row.style.flexDirection = 'row';
+    row.style.justifyContent = 'center';
+    // row.style.height = 'auto';
+    container.appendChild(row)
+    for (let j = 0; j < size; j++) {
+        let cell = document.createElement('div');
+        cell.className = 'el';
+        cell.style.border = 'solid';
+        cell.style.borderWidth = 'thin';
+        cell.style.flex = 'auto';
+        // cell.style.height = 'auto';
+        cell.innerText = '\n';
+        row.appendChild(cell);
+    }
+}
