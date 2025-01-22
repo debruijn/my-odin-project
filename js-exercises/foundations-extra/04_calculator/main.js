d044e0021f7859294107434b8dac1253b678cc0d
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-22 17:02:15 -0400

    Add working features to calculator

--- js-exercises/foundations-extra/04_calculator/main.js
+++ js-exercises/foundations-extra/04_calculator/main.js
@@ -16,10 +16,6 @@ const divide = function(a, b) {
 
 let num1 = 0, num2 = 0, op = add;
 
-const operate = function(op, num1, num2) {
-    return op(num1, num2)
-}
-
 
 const container = document.querySelector("#container");
 container.style.display = 'flex';
@@ -39,6 +35,7 @@ function createRow() {
     row.style.justifyContent = 'center';
     container.appendChild(row)
     row.style.fontSize = 'x-large';
+    row.style.textAlign = 'center';
     return row
 }
 
@@ -49,59 +46,153 @@ function createButton(text, row, mult=1, col='rgb(120 120 255)') {
     cell.style.border = 'solid';
     cell.style.borderWidth = '1px';
     cell.style.flex = 'none';
-    if (mult === 2) {
-        cell.style.width = '102px'
-    } else if (mult === 3) {
-        cell.style.width = '154px'
-    } else {
-        cell.style.width = '50px'
-    }
+    cell.style.width = 50 + 52 * (mult - 1) + 'px'
     cell.innerText = text;
     cell.style.backgroundColor = col;
     cell.style.alignContent = 'center';
-    cell.style.textAlign = 'center';
-    // cell.addEventListener("mouseover", changeColor, false);
+    cell.addEventListener("click", numPressed, false);
     row.appendChild(cell);
 }
 
 
+let numPressed = function(event) {
+    let txt = event.target.innerText;
+    if (Number(txt) + '' === txt) {
+        if (enterFirstNum) {
+            if (keepDisp) {
+                disp_prev.children[0].innerText = '';
+                disp_prev.children[2].innerText = '0';
+                keepDisp = false;
+            }
+            if (disp_prev.children[0].innerText === '0') {
+                disp_prev.children[0].innerText = '';
+            }
+            disp_prev.children[0].innerText += txt;
+        } else {
+            if (disp_prev.children[2].innerText === '0') {
+                disp_prev.children[2].innerText = '';
+            }
+            disp_prev.children[2].innerText += txt;
+        }
+    } else {
+        if (txt === '=') {
+            num1 = parseFloat(disp_prev.children[0].innerText);
+            if (enterFirstNum) {
+                lastRes = num1
+                disp.children[0].innerText = lastRes;
+                keepDisp = true;
+                return
+            }
+            num2 = parseFloat(disp_prev.children[2].innerText);
+            disp.children[0].innerText = op(num1, num2);
+            lastRes = op(num1, num2)
+            disp.children[0].innerText = Math.round(lastRes*10**8) / 10**8;
+            enterFirstNum = true;
+            keepDisp = true;
+            return
+        }
 
-function createGrid() {
+        if (txt === '<=') {
+            if (enterFirstNum) {
+                disp_prev.children[0].innerText = disp_prev.children[0].innerText.slice(0, -1)
+                if (disp_prev.children[0].innerText === '') {
+                    disp_prev.children[0].innerText = '0'
+                }
+            } else {
+                disp_prev.children[2].innerText = disp_prev.children[2].innerText.slice(0, -1)
+                if (disp_prev.children[2].innerText === '') {
+                    disp_prev.children[2].innerText = '0'
+                }
+            }
+            return
+        }
 
-    let disp = createRow();
-    createButton(num1, disp, 1, 'rgb(255 255 255)');
-    createButton('', disp,1, 'rgb(255 255 255)');
-    createButton(num2, disp, 2, 'rgb(255 255 255)');
-    disp.style.fontSize = 'xx-large';
+        if (txt === 'clr') {
+            enterFirstNum = true;
+            keepDisp = true;
+            disp_prev.children[1].innerText = '';
+            disp_prev.children[0].innerText = 0;
+            disp_prev.children[2].innerText = 0;
+            disp.children[0].innerText = 0;
+            return
+        }
 
-    let row = createRow();
-    createButton(7, row);
-    createButton(8, row);
-    createButton(9, row);
-    createButton('%', row, 1, 'rgb(180 180 180)');
+        if (keepDisp) {
+            disp_prev.children[0].innerText = Math.round(lastRes*10**8) / 10**8;
+            disp_prev.children[2].innerText = 0
+        }
+        if (enterFirstNum === false) {
+            num1 = parseFloat(disp_prev.children[0].innerText);
+            num2 = parseFloat(disp_prev.children[2].innerText);
+            lastRes = op(num1, num2)
+            disp.children[0].innerText = Math.round(lastRes*10**8) / 10**8;
+            disp_prev.children[0].innerText = Math.round(lastRes*10**8) / 10**8;
+            disp_prev.children[2].innerText = 0;
 
-    row = createRow();
-    createButton(4, row);
-    createButton(5, row);
-    createButton(6, row);
-    createButton('x', row, 1, 'rgb(180 180 180)');
-
-    row = createRow();
-    createButton(1, row);
-    createButton(2, row);
-    createButton(3, row);
-    createButton('-', row, 1, 'rgb(180 180 180)');
-
-    row = createRow();
-    createButton('<=', row, 1, 'rgb(255 120 120)');
-    createButton(0, row);
-    createButton('.', row, 1, 'rgb(180 180 180)');
-    createButton('+', row, 1, 'rgb(180 180 180)');
-
-    row = createRow();
-    createButton('clr', row, 1, 'rgb(255 120 120)');
-    createButton('=', row, 3, 'rgb(120 255 120)');
-
+        }
+        enterFirstNum = false;
+        disp_prev.children[1].innerText = txt
+        if (txt === '+') {
+            op = add
+        } else if (txt === '-') {
+            op = subtract
+        } else if (txt === 'x') {
+            op = multiply
+        } else if (txt === '%') {
+            op = divide
+        }
+    }
 }
 
-createGrid();
+let enterFirstNum = true;
+let keepDisp = true;
+let lastRes = 0;
+
+let disp_prev = createRow();
+createButton(num1, disp_prev, 1.75, 'rgb(255 255 255)');
+createButton('', disp_prev,0.5, 'rgb(255 255 255)');
+createButton(num2, disp_prev, 1.75, 'rgb(255 255 255)');
+
+let disp = createRow();
+createButton(num2, disp, 4, 'rgb(255 255 255)');
+disp.style.fontSize = 'xx-large';
+// disp.style.textAlign = 'right';
+// disp.children[0].style.paddingRight = '2px';
+
+let row = createRow();
+createButton(7, row);
+createButton(8, row);
+createButton(9, row);
+createButton('%', row, 1, 'rgb(180 180 180)');
+
+row = createRow();
+createButton(4, row);
+createButton(5, row);
+createButton(6, row);
+createButton('x', row, 1, 'rgb(180 180 180)');
+
+row = createRow();
+createButton(1, row);
+createButton(2, row);
+createButton(3, row);
+createButton('-', row, 1, 'rgb(180 180 180)');
+
+row = createRow();
+createButton('<=', row, 1, 'rgb(255 120 120)');
+createButton(0, row);
+createButton('.', row, 1, 'rgb(180 180 180)');
+createButton('+', row, 1, 'rgb(180 180 180)');
+
+row = createRow();
+createButton('clr', row, 1, 'rgb(255 120 120)');
+createButton('=', row, 3, 'rgb(120 255 120)');
+
+
+// Options:
+// - Num -> op -> Num -> = -> res
+// - Num -> op -> Num -> other op -> res but also res as new first num
+// - Op -> Num -> = -> res (take prev res
+// - Op -> Num -> other op -> res but also rest as new first num
+
+// State: enter num1 / enter num2 (start at: num1)
+// Press op immediate: shift curr res into num1 and enter num2 mode
